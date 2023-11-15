254a08bd55417f4134438d74022aa19e428eb92d
Andrii Lysak <andriilysak97@gmail.com>
2023-11-15 21:21:25 +0100

    calculator

--- DZ4/DZ4.py
+++ DZ4/DZ4.py
@@ -1,1 +1,15 @@
-print("dz4")
+x = int(input("Please enter first number:"))
+y = int(input("Please enter second number:"))
+
+
+action = (input("Please enter action(+, -, *, /) :"))
+if action == "+":
+    print("Your result is", x + y)
+elif action == "-":
+    print("Your result is", x - y)
+elif action == "*":
+    print("Your result is", x * y)
+elif action == "/" and y > 0:
+    print("Your result is", x / y)
+else:
+    print("You can't divide by 0")
