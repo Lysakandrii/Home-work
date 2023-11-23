78749c577361adf1c099dd553a732440609f40f2
Andrii Lysak <andriilysak97@gmail.com>
2023-11-23 22:15:52 +0100

    DZ12

--- DZ12/DZ12.py
+++ DZ12/DZ12.py
@@ -1,9 +1,9 @@
 import string
-initial_str = input('Enter your phrase to transform:')
+initial_str = str(input('Enter your phrase to transform:'))
 to_delete = string.punctuation+" "
-
+initial_str = initial_str.title()
 for l in initial_str:
     if l in to_delete:
         initial_str = initial_str.replace(l,"")
 
-print("#" + initial_str[:139].lower())
+print("#" + initial_str[:139])
