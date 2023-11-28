fb4f1f297f079188f74fb81bd76236c26441ce10
Andrii Lysak <andriilysak97@gmail.com>
2023-11-28 22:02:16 +0100

    DZ15.1.1

--- DZ15.1/DZ15.1.py
+++ DZ15.1/DZ15.1.py
@@ -1,4 +1,4 @@
-number = int(input("Please enter your number >0 and <8640000:"))
+number = int(input("Please enter your number >=0 and <8640000:"))
 end_dict = {'1':'день', '2':'дні', '3':'дні', '4':'дні', '5':'днів', '6':'днів', '7':'днів', '8':'днів', '9':'днів', '0':'днів'}
 days_number = str(number // 86400)
 hours = str(number % 86400 // 60 // 60)
